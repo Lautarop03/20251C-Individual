88d265078f6f27eed0d30e5d1dd2e686251d3eb5
Lautaro <lpedrozo@fi.uba.ar>
2025-03-11 18:08:14 -0300

    Add initial post /courses endpoint and logger

--- app.js
+++ app.js
@@ -1,11 +1,56 @@
 const express = require("express");
-const app = express()
-const port = 3000
+const winston = require("winston");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!\n')
+const app = express()
+const port = 3000 // ENV
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.simple()
+  ),
+  transports: [
+    new winston.transports.Console()
+  ]
+});
+
+// This middleware is for req.body to work
+app.use(express.json());
+
+// usar un uuid
+let id = 0;
+
+// Documenta funciones y clases siguiendo el estándar del lenguaje elegido.
+
+// Variables de entorno en otro archivo ¿.env?
+
+app.post('/courses', (req, res) => {
+  const {title, description} = req.body;
+
+  if (!title || !description) { // esta condicion es suficiente?
+    // Las respuestas de error deben seguir el RFC 7807 (**). Para este proyecto, por la complejidad, el campo type debe ser about:blank.
+    res.status(400).json(
+      {type: 'about:blank', 
+        title: 'Bad Request', 
+        status: 400, 
+        detail: 'Title and description are required', 
+        instance: req.url}
+    );
+
+    logger.error('Post /courses request without title or description');
+    return;
+  }
+
+  // persistir en memoria
+
+  id++; // uuid
+  const data = {id, title, description};
+
+  res.status(201).json(data);
+  logger.info(`Course created: ${title}`);
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  logger.info(`App listening on port ${port}`)
 })
