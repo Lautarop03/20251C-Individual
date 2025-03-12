06347faa5feb08228eaa5535ef32a65980b3403d
Lautaro <lpedrozo@fi.uba.ar>
2025-03-12 16:31:57 -0300

    test: post '/courses'

--- tests/app.test.js
+++ tests/app.test.js
@@ -3,17 +3,34 @@ const app = require("../app");
 require("dotenv").config();
 
 describe("POST /courses", () => {
-    it("should return body with title and description", async () => {
+    it("should create a new course and return 201 with course data", async () => {
       const res = await request(app)
         .post("/courses")
         .send({
           title: "Test Course",
-          description: "Test Description",
+          description: "Test Description"
         });
         
       expect(res.statusCode).toBe(201);
+      expect(res.body).toHaveProperty("data");
+      expect(res.body.data).toHaveProperty("id");
       expect(res.body.data.title).toBe("Test Course");
       expect(res.body.data.description).toBe("Test Description");
     });
+
+    it('should return 400 if the body is invalid', async () => {
+      const res = await request(app)
+        .post("/courses")
+        .send({
+          // 'title' is missing
+          description: "Course without a title"
+        });
+        
+      expect(res.statusCode).toBe(400);
+      expect(res.body).toHaveProperty('type');
+      expect(res.body).toHaveProperty('title');
+      expect(res.body).toHaveProperty('status');
+      expect(res.body).toHaveProperty('detail');
+      expect(res.body).toHaveProperty('instance');
 });
-  
+});
